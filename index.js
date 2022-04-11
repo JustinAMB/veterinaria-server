0446589d6b994ff3d47f508c6e5d9f8dc3a12bab
JustinAMB <Justinmb@outlook.es>
2022-04-10 23:30:00 -0600

    se llama a la base de datos

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import conectarDB from './db/config.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+conectarDB();
+
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
