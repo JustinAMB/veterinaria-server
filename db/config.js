28d7f09ce9f6a1dbba1c34f8776cd3b3ac0b3de6
JustinAMB <Justinmb@outlook.es>
2022-04-10 23:29:14 -0600

    e cambia el nombre a la variablede entorno

--- db/config.js
+++ db/config.js
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose';
 
-
-import dotenv from 'dotenv';
 const connectDB = async() => {
     try {
-        const db = await mongoose.connect(process.env.DB_MONGO, {
+        const db = await mongoose.connect(process.env.DB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
         const url = `${db.connection.host}: ${db.connection.port}`;
         console.log(`MongoDB connected: ${url}`);
+
     } catch (error) {
         console.log(`error: ${error.message}`);
         //process.exit(1);
